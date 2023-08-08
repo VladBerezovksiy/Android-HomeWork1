c4661bdec87f72fb0a2375f8c673541499d2b361
Vlad Berezovskyi <berezovskyijob@gmail.com>
2023-08-08 21:05:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.homework1
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+        val numberText: TextView = findViewById(R.id.number)
+        val minusButton: Button = findViewById(R.id.decreaseButton)
+        val plusButton: Button = findViewById(R.id.increaseButton)
+
+        plusButton.setOnClickListener {
+            count++
+            Toast.makeText(this, "Count: $count", Toast.LENGTH_SHORT).show()
+            numberText.setText("" + count)
+        }
+
+        minusButton.setOnClickListener {
+            if (count > 1) {
+                count--
+                Toast.makeText(this, "Count: $count", Toast.LENGTH_SHORT).show()
+                numberText.setText("" + count)
+            } else {
+                Toast.makeText(this, "Count cannot be less than 1", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+}
